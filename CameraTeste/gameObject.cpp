800410b61a3d84f94a89a8df97b4d31d8ed6b792
Arthur020104 <arthur020104@gmail.com>
2024-10-28 10:25:46 -0300

    Adicionando calculo de normais adicao de cores/luz

--- CameraTeste/gameObject.cpp
+++ CameraTeste/gameObject.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <glad/glad.h>
 #include "Libs/stb_image.h"
+#include "transformController.h"
 #include "shader.h"
 #include <iostream>
 #include "gameObject.h"
@@ -9,6 +10,10 @@
 #include <glm/glm/gtc/type_ptr.hpp>
 #include <algorithm>
 #include <functional>
+#include "main.h"
+#include <glm/glm/gtx/string_cast.hpp>
+#include <set>
+
 void generateTexture1(unsigned int& texture, const char* path)
 {
     int width, height, nrChannels;
@@ -39,35 +44,60 @@ void generateTexture1(unsigned int& texture, const char* path)
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-
-GameObject::GameObject(glm::vec3 inicialPos, glm::vec3 inicialRot, const float* vertices, const int& sizeOfVertices, Shader* shaderProgramRef)
+GameObject::GameObject(const glm::vec3& inicialPos, const glm::vec3& inicialRot, const glm::vec4& color, const float* vertices, const int& numVertices, Shader* shaderProgramRef, const bool& isStatic) : TransformController(inicialPos, inicialRot)
 {
-	objPos = inicialPos;
-	objRot = inicialRot;
+    objectColor = color;
+
     shaderProgram = shaderProgramRef;
-    verticesNum = sizeOfVertices / sizeof(float);
+    isStaticObj = isStatic;
+    
 
-    sizeOfVerticesObj = sizeOfVertices;
-    verticesObj = new float[sizeOfVertices];
-    std::copy(vertices, vertices + sizeOfVertices, verticesObj);
+    
+    verticesNum = numVertices;
+    sizeOfVerticesObj = sizeof(vertices[0]) * verticesNum;
 
+
+    verticesObj = new float[verticesNum]; 
+
+    std::copy(vertices, vertices + verticesNum, verticesObj);
+
+    // Resize vertex normals based on verticesNum
+    vertexNormals.resize(3*verticesNum, 0.0f);
+    
+    updateNormals();
+    
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeOfVertices, vertices, GL_STATIC_DRAW);
+    //Could be sizeOfVertices + sizeOfVertices because is the same size but doing vertexNormals.size() * sizeof(float) to be explicit
+    int sizeOfNormals = vertexNormals.size() * sizeof(float);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeOfVerticesObj + sizeOfNormals, nullptr, isStatic ? GL_STATIC_DRAW : GL_DYNAMIC_DRAW);
+
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeOfVerticesObj, verticesObj);
+
+
+    glBufferSubData(GL_ARRAY_BUFFER, sizeOfVerticesObj, sizeOfNormals, &vertexNormals[0]);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(sizeOfVerticesObj));
+    glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
 
 }
+
 GameObject::~GameObject()
 {
+    for (int textureId : texturesIds)
+    {
+        glDeleteTextures(1, (GLuint *)&textureId);
+    }
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     delete[] verticesObj;
@@ -77,21 +107,28 @@ void GameObject::setTextures(const float* textureCoords, const int& sizeOftextur
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-   
-    glBufferData(GL_ARRAY_BUFFER, sizeOfVerticesObj + sizeOftextureCoords, nullptr, GL_STATIC_DRAW);
+    int sizeOfNormals = vertexNormals.size() * sizeof(float);
 
+    glBufferData(GL_ARRAY_BUFFER, sizeOfVerticesObj + sizeOfNormals + sizeOftextureCoords, nullptr, isStaticObj ? GL_STATIC_DRAW : GL_DYNAMIC_DRAW);
 
     glBufferSubData(GL_ARRAY_BUFFER, 0, sizeOfVerticesObj, verticesObj);
 
+    glBufferSubData(GL_ARRAY_BUFFER, sizeOfVerticesObj, sizeOfNormals, &vertexNormals[0]);
 
-    glBufferSubData(GL_ARRAY_BUFFER, sizeOfVerticesObj, sizeOftextureCoords, textureCoords);
-
+    glBufferSubData(GL_ARRAY_BUFFER, sizeOfVerticesObj + sizeOfNormals, sizeOftextureCoords, textureCoords);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)(sizeOfVerticesObj));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(sizeOfVerticesObj));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)(sizeOfVerticesObj + sizeOfNormals));
+    glEnableVertexAttribArray(2);
+   
+   
+
+    
 
     texturesIds.reserve(numberOfTextures);
 
@@ -127,62 +164,29 @@ void GameObject::setTextures(const float* textureCoords, const int& sizeOftextur
    
     glBindVertexArray(0);
 }
-void GameObject::prepareRender()
+void GameObject::prepareRender(glm::mat4& view, const glm::mat4& projection)
 {
     shaderProgram->use();
+
+    this->shaderProgram->setMat4("projection", projection);
+    this->shaderProgram->setMat4("view", view);
+
+    glm::mat4 model = this->getModelMatrix();
+    this->shaderProgram->setMat4("model", model);
+
+    this->shaderProgram->setMat3("model3", glm::mat3(model));
+    this->shaderProgram->setMat3("view3", glm::mat3(view));
+
+    shaderProgram->setVec4("color", objectColor);
+
+    this->applyLighting();
+
     for (int i = 0; i < texturesIds.size(); i++)
     {
-        glActiveTexture(GL_TEXTURE0+i);
+        glActiveTexture(GL_TEXTURE0 + i);
         glBindTexture(GL_TEXTURE_2D, texturesIds[i]);
     }
     glBindVertexArray(VAO);
-    
-}
-glm::mat4 GameObject::getModelMatrix()
-{
-    glm::mat4 objPosMat = glm::translate(glm::mat4(1.0f), objPos);
-
-    glm::mat4 objRotMat = glm::rotate(glm::mat4(1.0f), glm::radians(static_cast<float>(objRot.z)), glm::vec3(0.0f, 0.0f, 1.0f));//rotate around x
-    objRotMat = glm::rotate(objRotMat, glm::radians(static_cast<float>(objRot.y)), glm::vec3(0.0f, 1.0f, 0.0f));//rotate around y
-    objRotMat = glm::rotate(objRotMat, glm::radians(static_cast<float>(objRot.x)), glm::vec3(1.0f, 0.0f, 0.0f));//rotate around z
-
-    glm::mat4 objScaleMat = glm::scale(glm::mat4(1.0f), objScale);
-    
-    return  objPosMat * objRotMat * objScaleMat;
-}
-glm::vec3 GameObject::getPos()
-{
-    return objPos;
-}
-glm::vec3 GameObject::getRot()
-{
-    return objRot;
-}
-glm::vec3 GameObject::getScale()
-{
-    return objScale;
-}
-
-void GameObject::setPos(const glm::vec3& newPos)
-{
-    objPos = newPos;
-}
-void GameObject::setRot(const glm::vec3& newRot)
-{
-    objRot = newRot;
-}
-void GameObject::setScale(const glm::vec3& newScale)
-{
-    objScale = newScale;
-}
-
-glm::vec3 GameObject::forward()
-{
-    return glm::vec3(
-        glm::cos(objRot.x) * glm::sin(objRot.y),  
-        glm::sin(objRot.x),                       
-        glm::cos(objRot.x) * glm::cos(objRot.y) 
-    );
 }
 void GameObject::setUpdateFunc(std::function<void(GameObject*)> func)
 {
@@ -190,6 +194,155 @@ void GameObject::setUpdateFunc(std::function<void(GameObject*)> func)
 }
 void GameObject::objUpdate()
 {
-    updateFunc(this);
+    if (updateFunc != nullptr)
+    {
+        updateFunc(this);
+    }
+    
+}
+bool containsNaN(const glm::vec3& vec) 
+{
+    return std::isnan(vec.x) || std::isnan(vec.y) || std::isnan(vec.z);
+}/*
+void GameObject::recalculateWVectors()
+{
+    wValues.clear();
+    wValues.reserve(sizeOfLights);
+    wValues.resize(sizeOfLights);
+    
+    glm::vec3 pos = this->getPos();
+
+    for (int i = 0; i < sizeOfLights; i++)
+    {
+        wValues[i] = (glm::length(lightsArr[i].getW(pos)) > 0 || containsNaN(lightsArr[i].getW(pos)) )? lightsArr[i].getW(pos) : -pos;
+        //std::cout << "global" << "\n";
+       // printVec3(wValues[i]);
+        //std::cout << "local" << "\n";
+        //printVec3(lightsArr[i].getW(getPos()));
+    }
+}*/
+void GameObject::updateLighting(Light* l, const int& sizeOfLightArr)//should be done by the scene and just kepp the w vector
+{
+    // Store the light array and size
+    lightsArr = l;
+    sizeOfLights = sizeOfLightArr;
+
+    lightColors.clear();
+    lightColors.reserve(sizeOfLights);
+
+    wValues.clear();
+    wValues.reserve(sizeOfLights);
+
+    lightIntensity.clear();
+    lightIntensity.reserve(sizeOfLights);
+
+    lightModelMatrix.clear();
+    lightModelMatrix.reserve(sizeOfLights);
+
+    lightColors.resize(sizeOfLights);
+    wValues.resize(sizeOfLights);
+    lightIntensity.resize(sizeOfLights);
+    lightModelMatrix.resize(sizeOfLights);
+
+    glm::vec3 pos = this->getPos();
+
+    for (int i = 0; i < sizeOfLights; i++)
+    {
+        wValues[i] = (glm::length(lightsArr[i].getW(pos)) > 0 || containsNaN(lightsArr[i].getW(pos))) ? lightsArr[i].getW(pos) : -pos;
+        lightColors[i] = lightsArr[i].getColor();
+        lightIntensity[i] = lightsArr[i].getIntensity();
+        lightModelMatrix[i] = glm::mat3(lightsArr[i].getModelMatrix());
+    }
 }
 
+
+
+void GameObject::applyLighting()
+{
+    shaderProgram->use();
+    /*This will be changed will be values of the scene not gameobject*/
+    shaderProgram->setInt("numberOfLights", sizeOfLights); //deve ser passado apenas uma vez para shader 
+    
+
+    shaderProgram->setFloatArray("lightIntensity", &lightIntensity[0], lightIntensity.size()); //deve ser passado apenas uma vez para shader
+
+    shaderProgram->setArrayVec3("lightsColorValues", &lightColors[0], lightColors.size()); //deve ser passado apenas uma vez para shader
+
+
+    shaderProgram->setArrayMat3("lightModelMat", &lightModelMatrix[0], lightModelMatrix.size());
+    /*This will be changed will be values of the scene not gameobject*/
+    
+    
+    shaderProgram->setArrayVec3("lightsWValues", &wValues[0], wValues.size());//deve ser passado para cada objeto no update do shader
+
+
+}
+struct Vec3Compare {
+    bool operator()(const glm::vec3& a, const glm::vec3& b) const {
+        if (a.x != b.x) return a.x < b.x;
+        if (a.y != b.y) return a.y < b.y;
+        return a.z < b.z;
+    }
+};
+
+void GameObject::updateNormals()
+{
+    std::fill(vertexNormals.begin(), vertexNormals.end(), 0.0f); // Reset normals
+
+    // Interior point for the object (assuming the object is centered at the origin)
+    glm::vec3 interiorPoint = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    for (int i = 0; i < verticesNum; i += 9)
+    {
+        glm::vec3 v1 = glm::vec3(verticesObj[i], verticesObj[i + 1], verticesObj[i + 2]);
+        glm::vec3 v2 = glm::vec3(verticesObj[i + 3], verticesObj[i + 4], verticesObj[i + 5]);
+        glm::vec3 v3 = glm::vec3(verticesObj[i + 6], verticesObj[i + 7], verticesObj[i + 8]);
+
+        glm::vec3 normal = glm::cross(v2 - v1, v3 - v1);
+
+        if (containsNaN(normal) || glm::length(normal) < 1e-6f)
+        {
+            normal = glm::vec3(0.0f, 0.0f, 1.0f);
+        }
+        else
+        {
+            normal = glm::normalize(normal);
+
+            glm::vec3 vectorToFace = v1 - interiorPoint;  
+            if (glm::dot(vectorToFace, normal) < 0.0f)
+            {
+                normal = -normal;
+            }
+        }
+
+        vertexNormals[i] = normal.x;
+        vertexNormals[i + 1] = normal.y;
+        vertexNormals[i + 2] = normal.z;
+
+        vertexNormals[i + 3] = normal.x;
+        vertexNormals[i + 4] = normal.y;
+        vertexNormals[i + 5] = normal.z;
+
+        vertexNormals[i + 6] = normal.x;
+        vertexNormals[i + 7] = normal.y;
+        vertexNormals[i + 8] = normal.z;
+    }
+
+
+    for (int i = 0; i < vertexNormals.size(); i += 3)
+    {
+        glm::vec3 accumulatedNormal = glm::vec3(vertexNormals[i], vertexNormals[i + 1], vertexNormals[i + 2]);
+        glm::vec3 normalizedNormal = glm::normalize(accumulatedNormal);
+
+        vertexNormals[i] = normalizedNormal.x;
+        vertexNormals[i + 1] = normalizedNormal.y;
+        vertexNormals[i + 2] = normalizedNormal.z;
+    }
+
+
+    // Atualizar dados do buffer
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeOfVerticesObj, vertexNormals.size() * sizeof(float), &vertexNormals[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
--- CameraTeste/gameObject.h
+++ CameraTeste/gameObject.h
@@ -2,52 +2,76 @@
 #include <glad/glad.h>
 #include <glm/glm/gtc/matrix_transform.hpp>
 #include <vector>
+#include "transformController.h"
 #include "shader.h"
+#include "light.h"
 #include <functional>
-class GameObject
+
+const glm::vec4 DEFAULT_COLOR = glm::vec4(0.1f, 0.1f, 0.1f, 1.0f);
+const bool DEFAULT_STATIC = true;
+const glm::vec3 DEFAULT_POSITION = glm::vec3(0.0f, 0.0f, 0.0f);
+const glm::vec3 DEFAULT_ROTATION = glm::vec3(0.0f, 0.0f, 0.0f);
+class GameObject:public TransformController
 {
 public:
 	unsigned int VAO;
 	unsigned int VBO;
 	Shader* shaderProgram;
 	int verticesNum;
+
+	glm::vec4 objectColor;
+
 	//unsigned int EBO;//can be automatcly created do it later, use a static method of gameobject
 
-	GameObject(glm::vec3 inicialPos, glm::vec3 inicialRot, const float* vertices, const int& sizeOfVertices, Shader* shaderProgramRef);
+	GameObject(const glm::vec3& inicialPos, const glm::vec3& inicialRot,const glm::vec4 &color, const float* vertices, const int& numVertices, Shader* shaderProgramRef, const bool& isStatic);
+	//Camera(glm::vec3 inicialPos, bool isFpsCam) : Camera(inicialPos, glm::vec3(0, 0, FAR_FRUSTUM), isFpsCam) {}
+	GameObject(const glm::vec3& inicialPos, const glm::vec3& inicialRot, const float* vertices, const int& numVertices, Shader* shaderProgramRef, const bool& isStatic) : GameObject(inicialPos, inicialRot, DEFAULT_COLOR, vertices, numVertices, shaderProgramRef, isStatic) {}
+
+	GameObject(const glm::vec3 &inicialPos,const glm::vec3 &inicialRot, const float* vertices, const int& numVertices, Shader* shaderProgramRef): GameObject(inicialPos,inicialRot,DEFAULT_COLOR, vertices, numVertices, shaderProgramRef, DEFAULT_STATIC) {}
+
+	GameObject(const glm::vec3 &inicialPos, const float* vertices, const int& numVertices, Shader* shaderProgramRef) : GameObject(inicialPos, DEFAULT_ROTATION, DEFAULT_COLOR, vertices, numVertices, shaderProgramRef, DEFAULT_STATIC) {}
+
+	GameObject(const float* vertices, const int& numVertices, Shader* shaderProgramRef) : GameObject(DEFAULT_POSITION, DEFAULT_ROTATION, DEFAULT_COLOR, vertices, numVertices, shaderProgramRef, DEFAULT_STATIC) {}
 
 
 	~GameObject();
-	glm::vec3 forward();
-
-	glm::vec3 right();
-
-	glm::vec3 getPos();
-	glm::vec3 getRot();
-	glm::vec3 getScale();
-
-	void setPos(const glm::vec3& newPos);
-	void setRot(const glm::vec3& newRot);
-	void setScale(const glm::vec3& newScale);
 
 	void objUpdate();
 	void setUpdateFunc(std::function<void(GameObject*)> func);
 
-	glm::mat4 getModelMatrix();
-
-	void prepareRender();
+	void prepareRender(glm::mat4& view, const glm::mat4& projectio);
 	void setTextures(const float* textureCoords, const int& sizeOftextureCoords, const char** paths, const int& numberOfTextures);
 	
+	void updateLighting(Light* l, const int &sizeOfLightArr);
 
+	
+	void updateNormals();
+
+	void recalculateWVectors();
 private:
-	glm::vec3 objPos;
-	glm::vec3 objRot;
-	glm::vec3 objScale = glm::vec3(1.0f,1.0f,1.0f);
 
+	std::vector<float> vertexNormals;
+	
+	int sizeOfVerticesObj;
+	float* verticesObj;
+
+	Light* lightsArr;
+	int sizeOfLights;
+	std::vector<glm::vec3> wValues;
+	/*This will be changed will be values of the scene not gameobject*/
+	std::vector<glm::vec3> lightColors;
+	std::vector<float> lightIntensity;
+	std::vector<glm::mat3> lightModelMatrix;
+	/*This will be changed will be values of the scene not gameobject*/
+
+	bool isStaticObj;
+
+	std::vector<unsigned int> texturesIds;
+	
+	
 	std::function<void(GameObject*)> updateFunc;
 
-	glm::vec3 forwardVec;
-	glm::vec3 rightVec;
-	std::vector<unsigned int> texturesIds;//add vector of bleding value
-	int sizeOfVerticesObj;
-	float *verticesObj;
+	void applyLighting();
+
+
 };
